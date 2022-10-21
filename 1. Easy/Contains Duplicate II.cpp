07bc0c7eb18b9df0e51946cd55ecf730acc3b145
Saidul Islam <fadil.cse5.bu@gmail.com>
2022-10-21 11:02:49 +0600

    Create Contains Duplicate II.cpp

---
+++ 1. Easy/Contains Duplicate II.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        map <int, int> mp;
+        for(int i = 0; i < nums.size(); i++) {
+            if(mp[nums[i]] > 0 and i - mp[nums[i]] + 1 <= k) return true;
+            mp[nums[i]] = i + 1;
+        }
+        return false;
+    }
+};
